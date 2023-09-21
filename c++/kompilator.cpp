4a2a1ae1278151aeebcc6a57f35e7780d44dc6e7
achi1_ <97097188+GOOFIE2137TDC@users.noreply.github.com>
2023-09-21 05:57:57 +0000

    update

--- c++/kompilator.cpp
+++ c++/kompilator.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 using namespace std;
 
-int bolkan(int mooscles, int poomps){
-    cin>>mooscles;
-    if poomps = 1
-    return "oh yiss my muscles are going stronker";
-    else "chuj ci w dupe złamasie jebany"
+int main() {
+    int telefon = 5;
+    int wsk;
+
+    cout<<"Wartosc zmiennej telefon: "<<telefon<<endl;
 }
