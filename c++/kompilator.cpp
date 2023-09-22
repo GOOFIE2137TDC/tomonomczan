87c05c648ac2fac5564aab0a9c15c5bfe2332437
achi1_ <97097188+GOOFIE2137TDC@users.noreply.github.com>
2023-09-22 07:54:25 +0000

    update

--- c++/kompilator.cpp
+++ c++/kompilator.cpp
@@ -1,21 +1,37 @@
 #include <iostream> 
 using namespace std; 
 
-void zmien(int* x, int* y) 
-{ 
-    int pomoc;
-    pomoc = *x;
-    *x = *y;
-    *y = pomoc;
-    return
+int sortowanie(int *a, int *b, int *c)
+{
+    int *wsk = &a;
+    int *wsk2 = &b;
+    int *wsk3 = &c;
+
+    if(*wsk > *wsk2)
+    {
+
+    }
+    if(*wsk > *wsk3)
+    {
+        
+    }
+    if(*wsk2 > *wsk3)
+    {
+        
+    }
+    return *wsk, *wsk2, *wsk3;
 }
 
 int main() 
 { 
-    int a = 5, b = 7;
-    int* wska = &a;
-    int* wskb = &b;
-    zmien(wska, wskb);
-    cout<<"a = "<<a<<", b = "<<b;
+    int a = 3, b = 2, c =1;
+    int *wsk = &a;
+    int *wsk2 = &b;
+    int *wsk3 = &c;
+
+    sortowanie(wsk, wsk2, wsk3);
+    cout<<sortowanie(wsk, wsk2, wsk3)<<endl;
     return 0; 
 } 
+
+ 
