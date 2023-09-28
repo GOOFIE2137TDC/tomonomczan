cd1db7593a37c586892c0bc4f4debfc7fd731fc8
achi1_ <97097188+GOOFIE2137TDC@users.noreply.github.com>
2023-09-28 06:31:46 +0000

    update

--- c++/kompilator.cpp
+++ c++/kompilator.cpp
@@ -1,37 +1,20 @@
 #include <iostream> 
 using namespace std; 
 
-int sortowanie(int *a, int *b, int *c)
-{
-    int *wsk = &a;
-    int *wsk2 = &b;
-    int *wsk3 = &c;
-
-    if(*wsk > *wsk2)
-    {
-
-    }
-    if(*wsk > *wsk3)
-    {
-        
-    }
-    if(*wsk2 > *wsk3)
-    {
-        
-    }
-    return *wsk, *wsk2, *wsk3;
-}
-
 int main() 
 { 
-    int a = 3, b = 2, c =1;
-    int *wsk = &a;
-    int *wsk2 = &b;
-    int *wsk3 = &c;
+    float a;
+    float as;
+    float* wsk = &a;
+    cout<<"podaj wartość zmiennej a: ";
+    cin>>a;
+    as = *wsk * 2;
 
-    sortowanie(wsk, wsk2, wsk3);
-    cout<<sortowanie(wsk, wsk2, wsk3)<<endl;
+    
+    cout<<"wartośc wskaźnika wsk: "<<*wsk; 
+    cout<<"\nwartość zmiennej as: "<<as;
     return 0; 
 } 
+
 
  
