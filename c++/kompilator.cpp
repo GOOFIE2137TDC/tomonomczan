cd7af5e04d06fd6919faa3739850322627e3d1f7
achi1_ <97097188+GOOFIE2137TDC@users.noreply.github.com>
2023-10-02 13:54:51 +0000

    update

--- c++/kompilator.cpp
+++ c++/kompilator.cpp
@@ -1,20 +1,8 @@
 #include <iostream> 
 using namespace std; 
 
-int main() 
-{ 
-    float a;
-    float as;
-    float* wsk = &a;
-    cout<<"podaj wartość zmiennej a: ";
-    cin>>a;
-    as = *wsk * 2;
 
-    
-    cout<<"wartośc wskaźnika wsk: "<<*wsk; 
-    cout<<"\nwartość zmiennej as: "<<as;
-    return 0; 
-} 
+
 
 
  
