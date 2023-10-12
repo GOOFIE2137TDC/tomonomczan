1e1ef66dcc466b98f3c735c2a4f628a78972a2aa
achi1_ <97097188+GOOFIE2137TDC@users.noreply.github.com>
2023-10-12 06:47:26 +0000

    update

--- c++/kompilator.cpp
+++ c++/kompilator.cpp
@@ -1,15 +1,3 @@
-#include <iostream>
-
+#incldue <iostream>
 using namespace std;
 
-int* zarezerwujePamiec()
-{
-    int* wskaznik = new int;
-    return wskaznik;
-}
-
-int main()
-{
-    int* zmienna;
-    zmienna = zarezerwujePamiec();
-}
