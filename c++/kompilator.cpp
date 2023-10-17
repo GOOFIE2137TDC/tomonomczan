7b925081cc25c74006c5cbf2910a030e0a3e74ff
achi1_ <97097188+GOOFIE2137TDC@users.noreply.github.com>
2023-10-17 13:23:02 +0000

    update

--- c++/kompilator.cpp
+++ c++/kompilator.cpp
@@ -1,1 +1,14 @@
- 
+#include <iostream>
+
+double* allocateMemoryAndSetValue() {
+    double* ptr = new double;
+    *ptr = 1.5;
+    return ptr;
+}
+
+int main() {
+    double* ptr = allocateMemoryAndSetValue();
+    std::cout << "The value of the pointer is: " << *ptr << std::endl;
+    delete ptr;
+    return 0;
+}
