d6385f523bca469735d809dd6783b7af5364a744
achi1_ <97097188+GOOFIE2137TDC@users.noreply.github.com>
2023-11-10 08:12:50 +0000

    update

--- c++/kompilator.cpp
+++ c++/kompilator.cpp
@@ -1,41 +1,11 @@
 #include <iostream>
 #include <string>
-#include <sstream>
-#include <vector>
-
 using namespace std;
 
-vector<string> podzielNaWyrazy(string tekst) {
-    vector<string> wyrazy;
-    stringstream ss(tekst);
-    string wyraz;
-    while (ss >> wyraz) {
-        wyrazy.push_back(wyraz);
-    }
-    return wyrazy;
-}
-
-vector<string> sortujWyrazy(vector<string> wyrazy) {
-    for (int i = 0; i < wyrazy.size(); i++) {
-        for (int j = i + 1; j < wyrazy.size(); j++) {
-            if (wyrazy[i] > wyrazy[j]) {
-                swap(wyrazy[i], wyrazy[j]);
-            }
-        }
-    }
-    return wyrazy;
-}
-
 int main() {
-    string tekst;
-    cout << "Podaj zdanie: ";
-    getline(cin, tekst);
-    vector<string> wyrazy = podzielNaWyrazy(tekst);
-    wyrazy = sortujWyrazy(wyrazy);
-    cout << "Wyrazy w kolejności alfabetycznej: ";
-    for (string wyraz : wyrazy) {
-        cout << wyraz << " ";
-    }
-    cout << endl;
+    string s = "skipper", t = "private";
+    int i = s.compare("kowalski") > 0;
+    int j = s.compare("rico") < 0;
+    cout<<i+j<<endl;
     return 0;
 }
