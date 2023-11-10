43758d8a5d1e9275c3daa74b24783b40dbfac5a3
achi1_ <97097188+GOOFIE2137TDC@users.noreply.github.com>
2023-11-10 07:39:06 +0000

    update

--- c++/kompilator.cpp
+++ c++/kompilator.cpp
@@ -1,23 +1,41 @@
 #include <iostream>
 #include <string>
+#include <sstream>
+#include <vector>
 
 using namespace std;
 
-string usunZnaki(string tekst) {
-    string wynik;
-    for (char c : tekst) {
-        if (c != ' ' && c != ',' && c != '.') {
-            wynik += c;
+vector<string> podzielNaWyrazy(string tekst) {
+    vector<string> wyrazy;
+    stringstream ss(tekst);
+    string wyraz;
+    while (ss >> wyraz) {
+        wyrazy.push_back(wyraz);
+    }
+    return wyrazy;
+}
+
+vector<string> sortujWyrazy(vector<string> wyrazy) {
+    for (int i = 0; i < wyrazy.size(); i++) {
+        for (int j = i + 1; j < wyrazy.size(); j++) {
+            if (wyrazy[i] > wyrazy[j]) {
+                swap(wyrazy[i], wyrazy[j]);
+            }
         }
     }
-    return wynik;
+    return wyrazy;
 }
 
 int main() {
     string tekst;
-    cout << "Podaj tekst: ";
+    cout << "Podaj zdanie: ";
     getline(cin, tekst);
-    string wynik = usunZnaki(tekst);
-    cout << "Tekst po usunieciu spacji, przecinkow i kropek: " << wynik << endl;
+    vector<string> wyrazy = podzielNaWyrazy(tekst);
+    wyrazy = sortujWyrazy(wyrazy);
+    cout << "Wyrazy w kolejności alfabetycznej: ";
+    for (string wyraz : wyrazy) {
+        cout << wyraz << " ";
+    }
+    cout << endl;
     return 0;
 }
