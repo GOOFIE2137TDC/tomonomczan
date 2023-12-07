18f16a980fca06d255f4be56f640ce9c2af7adfd
achi1_ <97097188+GOOFIE2137TDC@users.noreply.github.com>
2023-12-07 07:53:50 +0000

    update

--- c++/kompilator.cpp
+++ c++/kompilator.cpp
@@ -5,6 +5,8 @@
 
 using namespace std;
 
+
+
 class owoc
 {
     string nazwa;
@@ -22,13 +24,60 @@ class owoc
         ilosc = i;
         cena = c;
     }
-    string getnazwa(){
-        return nazwa;
+    string getnazwa();
+    int getilosc();
+    double getcena();
+
+    double wartosc(int ilosc);
+};
+
+int main()
+{
+    fstream magazyn;
+    magazyn.open("magazyn.txt", ios::in);
+    vector<owoc> owoce;
+    string linia;
+    string nazwa;
+    int ilosc;
+    double cena;
+
+
+    getline(magazyn, linia);
+    while (!magazyn.eof() == false)
+    {
+        magazyn >> nazwa >> ilosc >> cena;
+        owoc o(nazwa, ilosc, cena);
+        owoce.push_back(o);
     }
-    int getilosc(){
-        return ilosc;
-    }
-    double getcena(){
-        return cena;
-    }
+    
+    return 0;
+}
+
+owoc::owoc(string n, int i, double c) : nazwa(n), ilosc(i), cena(c) {}
+
+void owoc::setowoc(string n, int i, double c)
+{
+    this-> nazwa = n;
+    this-> ilosc = i;
+    this-> cena = c;
+}
+
+string owoc::getnazwa()
+{
+    return nazwa;
+}
+
+int owoc::getilosc()
+{
+    return ilosc;
+}
+
+double owoc::getcena()
+{
+    return cena;
+}
+
+double owoc::wartosc(int ilosc)
+{
+    return ilosc * cena;
 }
