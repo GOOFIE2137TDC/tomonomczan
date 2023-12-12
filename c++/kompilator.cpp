941aeccd042b650fb29a593d1a890ac995947c92
achi1_ <97097188+GOOFIE2137TDC@users.noreply.github.com>
2023-12-12 14:23:59 +0000

    update

--- c++/kompilator.cpp
+++ c++/kompilator.cpp
@@ -1,84 +1,20 @@
 #include <iostream>
-#include <vector>
 #include <cmath>
-#include <fstream>
 
 using namespace std;
 
 
-class owoc
+class prostokąt
 {
-    string nazwa;
-    int ilosc;
-    double cena;
-    
     public:
-    owoc(string n, int i, double c){
-        nazwa = n;
-        ilosc = i;
-        cena = c;
-    }
-    void setowoc(string n, int i, double c){
-        nazwa = n;
-        ilosc = i;
-        cena = c;
-    }
-    string getnazwa();
-    int getilosc();
-    double getcena();
-
-    double wartosc(int ilosc);
+    double a = 4;
+    double b = 8;
+    double pole = a*b;
+    double obw = 2*a + 2*b;
 };
 
 int main()
 {
-    fstream magazyn;
-    magazyn.open("magazyn.txt", ios::in);
-    vector<owoc> owoce;
-    string linia;
-    string nazwa;
-    int ilosc;
-    double cena;
-
-
-    getline(magazyn, linia);
-    while (!magazyn.eof() == false)
-    {
-        magazyn >> nazwa >> ilosc >> cena;
-        // owoc o(nazwa, ilosc, cena);
-        // owoce.push_back(o);
-        owoce.emplace_back(owoc(nazwa, ilosc, cena));
-    }
-    cout << owoce[3].getnazwa() << " " << owoce[3].wartosc(500);
-    
+    prostokąt p1;
     return 0;
-}
-
-owoc::owoc(string n, int i, double c) : nazwa(n), ilosc(i), cena(c) {}
-
-void owoc::setowoc(string n, int i, double c)
-{
-    this-> nazwa = n;
-    this-> ilosc = i;
-    this-> cena = c;
-}
-
-string owoc::getnazwa()
-{
-    return nazwa;
-}
-
-int owoc::getilosc()
-{
-    return ilosc;
-}
-
-double owoc::getcena()
-{
-    return cena;
-}
-
-double owoc::wartosc(int ilosc)
-{
-    return ilosc * cena;
 }
