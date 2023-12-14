6478b301ddf51ffe9f63278964f7f8d73e15d379
achi1_ <97097188+GOOFIE2137TDC@users.noreply.github.com>
2023-12-14 19:15:26 +0000

    update

--- c++/kompilator.cpp
+++ c++/kompilator.cpp
@@ -1,20 +1,28 @@
-#include <iostream>
-#include <cmath>
+#include <fstream>
+#include <cstdlib>
+#include <ctime>
+#include <string>
 
 using namespace std;
 
+string generateRandomNumber(int length) {
+    string result = "";
+    for(int i = 0; i < length; i++) {
+        result += to_string(rand() % 10);
+    }
+    return result;
+}
 
-class prostokąt
-{
-    public:
-    double a = 4;
-    double b = 8;
-    double pole = a*b;
-    double obw = 2*a + 2*b;
-};
+int main() {
+    srand(time(nullptr)); 
+    ofstream file("liczby.txt");
 
-int main()
-{
-    prostokąt p1;
+    for(int i = 0; i < 500; i++) {
+        int length = rand() % 11 + 30; 
+        string num = generateRandomNumber(length);
+        file << num << "\n";
+    }
+
+    file.close();
     return 0;
 }
