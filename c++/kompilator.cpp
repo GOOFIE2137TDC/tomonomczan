787bd7199effb86dac9d5d41fe99592bfbc3617f
achi1_ <97097188+GOOFIE2137TDC@users.noreply.github.com>
2024-02-08 07:52:12 +0000

    update

--- c++/kompilator.cpp
+++ c++/kompilator.cpp
@@ -2,40 +2,7 @@
 
 using namespace std;
 
-class obiekt
-{
-    public:
-    
-    int* liczba;
-    public:
-    obiekt(int parametr = 0){
-        *liczba = parametr;
-    }
-
-    obiekt(obiekt &orginal){
-        liczba = new int;
-        *liczba = *(orginal.liczba);
-    }
-
-    void wypisz(){
-        cout<<*liczba<<endl;
-
-    }
+class element {
+    int liczba;
+    int liczba = 0;
 };
-
-int main(){
-
-    obiekt a;
-    a.wypisz();
-
-    obiekt b(5);
-    b.wypisz();
-
-    obiekt c(b);
-    c.wypisz();
-
-    *(b.liczba) = 12;
-    b.wypisz();
-
-    return 0;
-}
