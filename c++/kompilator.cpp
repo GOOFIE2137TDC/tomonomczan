8c115180fc9243afdcf52f1d693e3052ee895cd6
achi1_ <97097188+GOOFIE2137TDC@users.noreply.github.com>
2024-02-09 08:19:58 +0000

    update

--- c++/kompilator.cpp
+++ c++/kompilator.cpp
@@ -1,12 +1,23 @@
 #include <iostream>
+#include <ctime>
+#include <vector>
 
 using namespace std;
 
 class element {
     int liczba;
-    int liczba = 0;
+    static int licznik;
+    
+    public:
+        void losuj() {
+           liczba = -100 + rand() % 201;
+           if (liczba % 2 == 0) {
+               licznik++;
+           }
+        }
 };
 
-int losuj(int a, int b) {
-    return rand() % (b - a + 1) + a;
+int element::licznik = 0;
+
+int main() {
 }
