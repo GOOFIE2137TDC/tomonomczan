dddecb5a330b541f7c3eb9ad2e027b8dbae35de5
achi1_ <97097188+GOOFIE2137TDC@users.noreply.github.com>
2024-02-13 14:25:00 +0000

    update

--- c++/kompilator.cpp
+++ c++/kompilator.cpp
@@ -4,19 +4,3 @@
 
 using namespace std;
 
-int main() {
-    vector<int> numbers(3);
-    cout << "Podaj trzy liczby:\n";
-    for (int i = 0; i < 3; i++) {
-        cin >> numbers[i];
-    }
-
-    sort(numbers.begin(), numbers.end(), greater<int>());
-
-    cout << "Liczby w porządku malejącym:\n";
-    for (int number : numbers) {
-        cout << number << "\n";
-    }
-
-    return 0;
-}
