f6cb7f52a1255e817a6317d177c9fcce2a39ffa9
achi1_ <97097188+GOOFIE2137TDC@users.noreply.github.com>
2024-02-15 07:51:50 +0000

    update

--- c++/kompilator.cpp
+++ c++/kompilator.cpp
@@ -1,72 +1,20 @@
-#include <iostream> 
-#include <iomanip> 
-#include <vector> 
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
 
-using namespace std; 
-
-class Element 
-{
-    int liczba;
+class Prostokat {
+    int x;
+    int y;
+    int szerokosc;
+    int wysokosc;
 
 public:
-    Element(int liczba){
-        this->liczba = liczba;
-        cout << "Element (" << liczba << ") utworzony." << endl;
+    Prostokat(int x, int y, int szerokosc, int wysokosc)
+        : x(x), y(y), szerokosc(szerokosc), wysokosc(wysokosc) {}
+
+    bool czyDotyka(const Prostokat& inny) {
+        return x < inny.x + inny.szerokosc && x + szerokosc > inny.x &&
+               y < inny.y + inny.wysokosc && y + wysokosc > inny.y;
     }
-
-    int get(){
-        return liczba;
-    }
-
-    void put(int liczba){
-        this->liczba = liczba;
-    }
-
-}; 
-
- 
-
-class Zbior 
-
-{
-    Element elem1, elem2;                 // Powołanie do życia obiektów innej klasy
-
-public: 
-
- 
-
-// lista konstruktorów Elementow ich kolejnosc nie jest istotna, 
-
-// kolejnosc konstruowania obiektów zależy od kolejności ich deklaracji, jak w linii powyżej (Element elem1, elem2)  
-
- 
-
-Zbior() : elem2(2), elem1(1) //lista konstruktorów Elementow
-{
-    cout << "Zbior Elementow utworzony." << endl;
-} 
-
-    int get(int elem_nmbr){
-        return elem_nmbr == 1 ? elem1.get() : elem2.get();
-    }
-
-    void put(int elNmbr, int liczba){
-        if (elNmbr == 1){
-            elem1.put(liczba);
-        } else {
-            elem2.put(liczba);
-        }
-    }
-
-
-}; 
-
- 
-
- 
-
-main(){
-    Zbior kolekcja;
-
-    return 0;
-} 
+};
