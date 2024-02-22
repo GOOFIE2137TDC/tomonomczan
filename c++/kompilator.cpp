10f7d01d8ac494786e9bcf225e74f664ffc4018f
achi1_ <97097188+GOOFIE2137TDC@users.noreply.github.com>
2024-02-22 07:39:19 +0000

    update

--- c++/kompilator.cpp
+++ c++/kompilator.cpp
@@ -12,19 +12,55 @@ class Prostokat {
     int wysokosc;
 
 public:
-    Prostokat() = default;
-    Prostokat(int px, int py, int ps, int pw)
-        : x(px), y(py), szerokosc(ps), wysokosc(pw) {}
+    Prostokat(int x, int y, int szerokosc, int wysokosc)
+        : x(x), y(y), szerokosc(szerokosc), wysokosc(wysokosc) {}
 
-    int getX() const { return x; }
-    int getY() const { return y; }
-
-
-    bool czyDotyka(Prostokat& inny) {
+    bool czyDotyka(const Prostokat& inny) const {
         return x < inny.x + inny.szerokosc && x + szerokosc > inny.x &&
                y < inny.y + inny.wysokosc && y + wysokosc > inny.y;
     }
+
+    void rysuj(vector<vector<char>>& siatka, char znak) const {
+        for (int i = x; i < x + szerokosc; i++) {
+            for (int j = y; j < y + wysokosc; j++) {
+                siatka[i][j] = znak;
+            }
+        }
+    }
 };
 
-int main(){
+int main() {
+    srand(time(0));
+
+    vector<Prostokat> prostokaty;
+    for (int i = 0; i < 5; i++) {
+        int x = rand() % 31;
+        int y = rand() % 31;
+        int szerokosc = 2 + rand() % 9;
+        int wysokosc = 2 + rand() % 9;
+        prostokaty.push_back(Prostokat(x, y, szerokosc, wysokosc));
+    }
+
+    for (int i = 0; i < prostokaty.size(); i++) {
+        for (int j = i + 1; j < prostokaty.size(); j++) {
+            if (prostokaty[i].czyDotyka(prostokaty[j])) {
+                cout << "Prostokat " << i << " dotyka prostokata " << j << endl;
+            }
+        }
+    }
+
+    vector<vector<char>> siatka(41, vector<char>(41, ' '));
+    char znaki[] = {'#', '*', '+', '@', '%'};
+    for (int i = 0; i < prostokaty.size(); i++) {
+        prostokaty[i].rysuj(siatka, znaki[i]);
+    }
+
+    for (int i = 0; i < siatka.size(); i++) {
+        for (int j = 0; j < siatka[i].size(); j++) {
+            cout << siatka[i][j];
+        }
+        cout << endl;
+    }
+
+    return 0;
 }
