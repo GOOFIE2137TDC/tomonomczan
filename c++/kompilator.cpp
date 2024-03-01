729572a552850f415ac7529d125dd78b79dda524
achi1_ <97097188+GOOFIE2137TDC@users.noreply.github.com>
2024-03-01 08:21:14 +0000

    update

--- c++/kompilator.cpp
+++ c++/kompilator.cpp
@@ -1,27 +1,46 @@
 #include <iostream>
-#include <stack>
 #include <vector>
 
 using namespace std;
 
 class Stos {
-    private:
-        stack<int> stos;
-        vector<int> max;
-        vector<int> min;
+private:
+    vector<int> stos;
 
-    public:
-        void push(int x) {
-            cin >> x;
-            stos.push(x);
-            if (max.empty() || x > max.back()) {
-                max.push_back(x);
-            } 
-            else {
-                max.push_back(max.back());
-            }
+public:
+    void push(int value) {
+        stos.push_back(value);
+    }
+
+    void pop() {
+        if (!stos.empty()) {
+            stos.pop_back();
+        } else {
+            cout << "Stos jest pusty.\n";
         }
+    }
+
+    int top() {
+        if (!stos.empty()) {
+            return stos.back();
+        } else {
+            cout << "Stos jest pusty.\n";
+            return -1;
+        }
+    }
+
+    bool empty() {
+        return stos.empty();
+    }
 };
 
 int main() {
+    Stos s;
+    s.push(1);
+    s.push(2);
+    s.push(3);
+    cout << s.top() << endl; 
+    s.pop();
+    cout << s.top() << endl; 
+    return 0;
 }
