719f48d74ad5720e9ada530923b692812ba3e3a6
achi1_ <97097188+GOOFIE2137TDC@users.noreply.github.com>
2024-03-15 12:57:09 +0000

    update

--- c++/kompilator.cpp
+++ c++/kompilator.cpp
@@ -1,47 +1,110 @@
-#include <iostream>
+#include <iostream> 
+#include <string> 
 
-using namespace std;
+using namespace std; 
 
 class Osoba {
-    string imie;
-    string nazwisko;
-    int nrDowodu;
+protected:
 
-public:
-    Osoba(string pimie, string pnazwisko, int pnrDowodu) {
-        imie{pimie}, nazwisko{pnazwisko}, nrDowodu{pnrDowodu}{
-            cout<<"Osoba\n";
-        }
-    }
-    ~Osoba(){
-        cout<<"Destruktor Osoba\n";
+    string imie; 
+    string nazwisko; 
+    int nrDowodu; 
+
+public: 
+
+    Osoba() : imie(""), nazwisko(""), nrDowodu(0) { 
+        cout << "Konstruktor domyślny Osoba\n"; 
+    } 
+
+    Osoba(string imie, string nazwisko, int nrDowodu) : imie(imie), nazwisko(nazwisko), nrDowodu(nrDowodu) { 
+        cout << "Konstruktor ogólny Osoba\n"; 
+    } 
+
+    ~Osoba() { 
+        cout << "Osoba_Destr\n"; 
+    } 
+
+    virtual void piszKimJestes() const { 
+        cout << "Osoba\n"; 
+    } 
+
+    virtual void wypiszDane() const { 
+
+        cout << "Imię: " << imie << ", Nazwisko: " << nazwisko << ", Nr dowodu: " << nrDowodu << "\n"; 
+    } 
+}; 
+
+
+class Student : public Osoba { 
+private: 
+    int nrIndeksu; 
+
+public: 
+
+    Student() : Osoba(), nrIndeksu(0) { 
+        cout << "Konstruktor domyślny Student\n"; 
     }
 
-    string getImie() {
-        return imie;
-    }
-    string getNazwisko() {
-        return nazwisko;
-    }
-    int getNrDowodu() {
-        return nrDowodu;
+    Student(string imie, string nazwisko, int nrDowodu, int nrIndeksu) : Osoba(imie, nazwisko, nrDowodu), nrIndeksu(nrIndeksu) { 
+        cout << "Konstruktor ogólny Student\n"; 
+    } 
+
+    ~Student() { 
+        cout << "Student_Destr\n"; 
     }
 
-    void wypisz() {
-        cout << "Imie: " << imie << endl;
-        cout << "Nazwisko: " << nazwisko << endl;
-        cout << "Nr dowodu: " << nrDowodu << endl;
+    void piszKimJestes() const override { 
+        cout << "Student\n"; 
+    } 
+
+    void wypiszDane() const override { 
+        Osoba::wypiszDane(); 
+        cout << "Nr indeksu: " << nrIndeksu << "\n"; 
+    }
+}; 
+
+
+class Absolwent : public Student {
+private: 
+    int nrDyplomu; 
+
+public: 
+
+    Absolwent() : Student(), nrDyplomu(0) { 
+        cout << "Konstruktor domyślny Absolwent\n"; 
+    } 
+
+    Absolwent(string imie, string nazwisko, int nrDowodu, int nrIndeksu, int nrDyplomu) : Student(imie, nazwisko, nrDowodu, nrIndeksu), nrDyplomu(nrDyplomu) {
+        cout << "Konstruktor ogólny Absolwent\n"; 
     }
 
-    void piszKimJestes() {
-        cout << "Osoba\n";
+    ~Absolwent() { 
+        cout << "Absolwent_Destr\n"; 
     }
-};
 
-int main(){
-    Osoba o;
+    int getNrDyplomu() const { 
+        return nrDyplomu; 
+    }
+
+    void piszKimJestes() const override { 
+        cout << "Absolwent\n"; 
+    } 
+
+    void wypiszDane() const override { 
+        Student::wypiszDane(); 
+        cout << "Nr dyplomu: " << nrDyplomu << "\n"; 
+    }
+}; 
+
+
+int main() {
+    Osoba o("Jan", "Kowalski", 123456789);
+    Student s("Anna", "Nowak", 987654321, 123456789);
+    Absolwent a("Piotr", "Zieliński", 123456789, 987654321, 123456);
+
     o.piszKimJestes();
-    o.wypisz();
-    return 0;
+    s.piszKimJestes();
+    a.piszKimJestes();
 
+    return 0;
 }
