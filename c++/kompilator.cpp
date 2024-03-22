bf6a18ddef97ae555fa1a75bffca3f44adc2c108
achi1_ <97097188+GOOFIE2137TDC@users.noreply.github.com>
2024-03-22 07:35:02 +0000

    update

--- c++/kompilator.cpp
+++ c++/kompilator.cpp
@@ -8,48 +8,47 @@ using namespace std;
 
 class Instrument {
 public:
-    virtual void graj() const = 0; // czysta funkcja wirtualna
+    virtual void graj() const = 0; 
 };
 
 class Gitara : public Instrument {
 public:
     void graj() const override {
-        cout << "Dźwięk gitary\n";
+        cout << "brzdęk brzdęk\n";
     }
 };
 
 class Pianino : public Instrument {
 public:
     void graj() const override {
-        cout << "Dźwięk pianina\n";
+        cout << "tun tun\n";
     }
 };
 
 class Skrzypce : public Instrument {
 public:
     void graj() const override {
-        cout << "Dźwięk skrzypiec\n";
+        cout << "skrzyp skrzyp\n";
     }
 };
 
 int main() {
-    srand(time(0)); // inicjalizacja generatora liczb losowych
+    srand(time(0));
 
     vector<Instrument*> orkiestra;
-    orkiestra.push_back(new Gitara());
-    orkiestra.push_back(new Pianino());
-    orkiestra.push_back(new Skrzypce());
+    orkiestra.push_back(new Gitara);
+    orkiestra.push_back(new Pianino);
+    orkiestra.push_back(new Skrzypce);
 
-    cout << "Koncert:\n";
-    for (int i = 0; i < 10; ++i) {
-        int indeks = rand() % orkiestra.size(); // losowanie indeksu
-        orkiestra[indeks]->graj();
+    cout << "Koncert wirtuozów: \n";
+    for int i = 0; i < 10; i++) {
+        orkiestra[rand() % orkiestra.size()]->graj();
     }
 
-    // zwolnienie pamięci
-    for (Instrument* instrument : orkiestra) {
-        delete instrument;
+    for (const auto& instrument : orkiestra) {
+        instrument->graj();
     }
+
 
     return 0;
 }
