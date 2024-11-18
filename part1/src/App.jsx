d0427fdcbc13631d41374b23548986ed26cb9201
Mateusz Pietrzyk <97097188+GOOFIE2137TDC@users.noreply.github.com>
2024-11-18 10:37:43 +0000

    update

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,36 +1,9 @@
-const Hello = (props) => {
-
-  console.log(props)
+const Content = ... {
   return (
     <div>
-      <p>
-
-        Hello {props.name}, you are {props.age} years old
-      </p>
+      <Part .../>
+      <Part .../>
+      <Part .../>
     </div>
   )
 }
-
-const footer = () => {
-  return (
-    <div>
-      greeting app created by <a href='https://github.com/mluukkai'>mluukkai</a>
-    </div>
-  )
-}
-
-const App = () => {
-  const friends = [
-    { name: 'Peter', age: 4 },
-    { name: 'Maya', age: 10 },
-  ]
-
-  return (
-    <div>
-      <p>{friends[0]}</p>
-      <p>{friends[1]}</p>
-    </div>
-  )
-}
-
-export default App
