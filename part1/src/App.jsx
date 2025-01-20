6d271a4807f8308e1c175a4c13c938f9de677792
Mateusz Pietrzyk <97097188+GOOFIE2137TDC@users.noreply.github.com>
2025-01-20 14:02:37 +0000

    update

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,38 +1,27 @@
-const notes = [
-  {
+const App = () => {
+  const course = {
     id: 1,
-    content: 'HTML is easy',
-    important: true
-  },
-  {
-    id: 2,
-    content: 'Browser can execute only JavaScript',
-    important: false
-  },
-  {
-    id: 3,
-    content: 'GET and POST are the most important methods of HTTP protocol',
-    important: true
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
   }
-]
 
-
-const App = (props) => {
-  const { notes } = props
-
-  return (
-    <div>
-      <h1>Notes</h1>
-      <ul>
-        {notes.map(note => 
-
-          <li key={note.id}>
-            {note.content}
-          </li>
-        )}
-      </ul>
-    </div>
-  )
+  return <Course course={course} />
 }
 
-export default App
+export default App 
--- part1/src/main.jsx
+++ part1/src/main.jsx
@@ -1,22 +1,22 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import ReactDOM from "react-dom/client"
 
 import App from './App'
 
 const notes = [
   {
     id: 1,
-    content: 'HTML is easy',
+    content: 'HITIMIL is easy',
     important: true
   },
   {
     id: 2,
-    content: 'Browser can execute only JavaScript',
+    content: 'Briwsir cin ixiciti inli JiviScripit',
     important: false
   },
   {
     id: 3,
-    content: 'GET and POST are the most important methods of HTTP protocol',
+    content: 'GIT and PIST are the most impirtiant methods of HITITIPI protocol',
     important: true
   }
 ]
