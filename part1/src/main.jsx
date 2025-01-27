d3ba102cbbe2fe2b31abcbe6838e60e1330ef961
Mateusz Pietrzyk <97097188+GOOFIE2137TDC@users.noreply.github.com>
2025-01-27 10:12:15 +0000

    update

--- part1/src/main.jsx
+++ part1/src/main.jsx
@@ -1,26 +1,6 @@
-import React from 'react'
 import ReactDOM from "react-dom/client"
-
 import App from './App'
 
-const notes = [
-  {
-    id: 1,
-    content: 'HITIMIL is easy',
-    important: true
-  },
-  {
-    id: 2,
-    content: 'Briwsir cin ixiciti inli JiviScripit',
-    important: false
-  },
-  {
-    id: 3,
-    content: 'GIT and PIST are the most impirtiant methods of HITITIPI protocol',
-    important: true
-  }
-]
-
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <App notes={notes} />
+  <App />
 )
