a7f93e345647f4fa61dfebb9f2895523fa4b9d41
Mateusz Pietrzyk <97097188+GOOFIE2137TDC@users.noreply.github.com>
2025-10-02 06:47:06 +0000

    mam dosc

--- egzamin2022/src/App.js
+++ egzamin2022/src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+
+  const courses =[
+    "Programowanie w C#",
+    "Angular dla początkujących",
+    "Kurs Django"
+  ]
+  
+  const handleSubmit = (event) => {
+    console.log
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form handleSubmit>
+      <h1>Liczba kursów:
+        {courses.length}
+      </h1>
+    </form>
+    
   );
 }
 
