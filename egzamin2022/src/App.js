75faa866a63a3ba5f1cc95707ddbda9aa0b3d302
Mateusz Pietrzyk <97097188+GOOFIE2137TDC@users.noreply.github.com>
2025-10-10 09:47:12 +0000

    update

--- egzamin2022/src/App.js
+++ egzamin2022/src/App.js
@@ -10,7 +10,27 @@ function App() {
     console.log(`tytul: ${title}, rodzaj: ${type}`);
 
   }
-
+  return (
+    <div className="container">
+      <form onSubmit={onSubmit}>
+        <div className="mb-3">
+          <label htmlFor="filmTitle" className="form-label">Tytuł filmu</label>   
+          <input type="text" className="form-control" id="filmTitle" name="filmTitle" />
+        </div>
+        
+        <div className="mb-3">
+    <label htmlFor="filmType" className="form-label">Rodzaj filmu</label>
+      <select className="form-select" id="filmType" name="filmType">
+        <option value="akcja">akcja</option>
+        <option value="komedia">komedia</option>
+        <option value="dramat">dramat</option>
+        <option value="horror">horror</option>
+      </select>
+    </div>
+      <button type="submit" className="btn btn-primary">Zapisz</button>
+      </form>
+    </div>
+  );
   
 
 }
