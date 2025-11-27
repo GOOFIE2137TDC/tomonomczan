da54ee5a18fb9bdf9a1205bf493e4a07820b9d79
Mateusz Pietrzyk <97097188+GOOFIE2137TDC@users.noreply.github.com>
2025-11-27 06:49:51 +0000

    mhkijnn

--- egzamin2022/src/App.js
+++ egzamin2022/src/App.js
@@ -10,6 +10,19 @@ function App() {
   const handleAddExpense = () => {
     console.log(`kategoria: ${category}; kwota: ${amount}; data: ${date}`);
     setExpenses([...expenses, { category, amount, date }]);
+    {
+      <>
+      <div className='container'>
+        <form>
+          <div className='form-group'>
+            <label>Kategoria wydatku:</label>
+            <input type='text' className='form-control' value={category} 
+            onChange={e => setCategory(e.target.value)} />
+          </div>
+        </form>
+      </div>
+      </>
+    }
+  }
 }
-
 export default App;
