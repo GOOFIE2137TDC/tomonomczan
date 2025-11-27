1c6d319f627f221d27ab7ecdfb21a063077037d9
Mateusz Pietrzyk <97097188+GOOFIE2137TDC@users.noreply.github.com>
2025-11-27 06:34:06 +0000

    update

--- egzamin2022/src/App.js
+++ egzamin2022/src/App.js
@@ -2,10 +2,14 @@ import 'bootstrap/dist/css/bootstrap.css'
 import { useState } from 'react'
 
 function App() {
-  
+  const [category, setCategory] = useState('');    
+  const [amount, setAmount] = useState('');
+  const [date, setDate] = useState('');
+  const [expenses, setExpenses] = useState([]);
 
-    
-
-
+  const handleAddExpense = () => {
+    console.log(`kategoria: ${category}; kwota: ${amount}; data: ${date}`);
+    setExpenses([...expenses, { category, amount, date }]);
 }
-export default App
+
+export default App;
